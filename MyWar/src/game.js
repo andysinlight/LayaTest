8cd0744af996a495755073e04cc942fe3b0bf69e
andysinligh <JUST@2do>
2018-04-24 22:13:28 +0800

    test http

--- MyWar/src/game.js
+++ MyWar/src/game.js
@@ -1,5 +1,4 @@
-﻿var Loader = laya.net.Loader;
-var Handler = laya.utils.Handler;
+﻿var Handler = laya.utils.Handler;
 
 //初始化微信小游戏
 Laya.MiniAdpter.init();
@@ -9,27 +8,41 @@ Laya.init(480, 852);
 //激活资源版本控制
 Laya.ResourceVersion.enable("version.json", Handler.create(null, beginLoad), Laya.ResourceVersion.FILENAME_VERSION);
 
-function beginLoad(){
+var baseURL = "http://localhost";
+var wsURL = "ws://192.168.1.155:9081";
+
+var httpIo = new HttpIo(baseURL);
+var xhr = httpIo.httpGet("/api/guest", function (data, object) {
+	console.log("game connect success!!");
+	console.log(data);
+}, function () {
+	console.log("game connect fail !!");
+});
+
+
+
+new SocketIo(wsURL);
+
+function beginLoad() {
 	Laya.loader.load("res/atlas/war.atlas", Handler.create(null, onLoaded));
 
 }
 
-function onLoaded()
-{
+function onLoaded() {
 	var bg = new background();
 	Laya.stage.addChild(bg);
 
-	this.hero =new Role();
-	this.hero.init("hero",0,100,0,4,0);
-    this.hero.pos(200,500);
+	this.hero = new Role();
+	this.hero.init("hero", 0, 100, 0, 4, 0);
+	this.hero.pos(200, 500);
 	Laya.stage.addChild(this.hero);
 
-	Laya.stage.on(Laya.Event.MOUSE_MOVE,this,onMouseMove);
+	Laya.stage.on(Laya.Event.MOUSE_MOVE, this, onMouseMove);
 
 }
 
-var onMouseMove = function(){
-	this.hero.pos(Laya.stage.mouseX,Laya.stage.mouseY);
+var onMouseMove = function () {
+	this.hero.pos(Laya.stage.mouseX, Laya.stage.mouseY);
 }
 
 
